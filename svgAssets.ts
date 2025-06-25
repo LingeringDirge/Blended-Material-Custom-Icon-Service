d4fd4f018cf2ad763bc50272cecd93b8431ea760
Vince Fermo <vincentfermo@gmail.com>
2025-06-25 12:11:52 -0500

    Create svgAssets.ts

---
+++ svgAssets.ts
@@ -0,0 +1,185 @@
+/** TODO: This file exists to avoid CORs loading issues.  May explore in the future a way around this. */
+
+const iconDir = 'assets/images/';
+
+/** These are the custom icons designed to be used as part of the esms icon component.
+ * These eventually get rendered as svg by the mat icon registry
+ */
+export const svgAssets = [
+    {
+        name: 'administration',
+        path: `${iconDir}administration.svg`
+    },
+    {
+        name: 'assessments',
+        path: `${iconDir}assessments.svg`
+    },
+    {
+        name: 'assessments_work',
+        path: `${iconDir}assessments-work.svg`
+    },
+    {
+        name: 'asset',
+        path: `${iconDir}asset.svg`
+    },
+    {
+        name: 'component',
+        path: `${iconDir}component.svg`
+    },
+    {
+        name: 'component_cost',
+        path: `${iconDir}component-cost.svg`
+    },
+    {
+        name: 'component_importance',
+        path: `${iconDir}component-importance.svg`
+    },
+    {
+        name: 'cost',
+        path: `${iconDir}cost.svg`
+    },
+    {
+        name: 'cost_modifiers',
+        path: `${iconDir}cost-modifiers.svg`
+    },
+    {
+        name: 'documents',
+        path: `${iconDir}documents.svg`
+    },
+    {
+        name: 'domains',
+        path: `${iconDir}domains.svg`
+    },
+    {
+        name: 'export',
+        path: `${iconDir}export.svg`
+    },
+    {
+        name: 'asset',
+        path: `${iconDir}asset.svg`
+    },
+    {
+        name: 'asset_filled',
+        path: `${iconDir}asset-filled.svg`
+    },
+    {
+        name: 'generalized_export',
+        path: `${iconDir}generalized_export.svg`
+    },
+    {
+        name: 'generalized_import',
+        path: `${iconDir}generalized_import.svg`
+    },
+    {
+        name: 'groups',
+        path: `${iconDir}groups.svg`
+    },
+    {
+        name: 'home',
+        path: `${iconDir}home.svg`
+    },
+    {
+        name: 'import',
+        path: `${iconDir}import.svg`
+    },
+    {
+        name: 'import_export_configuration',
+        path: `${iconDir}import-export-configuration.svg`
+    },
+    {
+        name: 'imports',
+        path: `${iconDir}imports.svg`
+    },
+    {
+        name: 'inflation',
+        path: `${iconDir}inflation.svg`
+    },
+    {
+        name: 'libraries',
+        path: `${iconDir}libraries.svg`
+    },
+    {
+        name: 'logo',
+        path: `${iconDir}logo.svg`
+    },
+    {
+        name: 'map_pin',
+        path: `${iconDir}map-pin.svg`
+    },
+    {
+        name: 'organizations',
+        path: `${iconDir}organizations.svg`
+    },
+    {
+        name: 'project',
+        path: `${iconDir}project.svg`
+    },
+    {
+        name: 'reports',
+        path: `${iconDir}reports.svg`
+    },
+    {
+        name: 'service_life',
+        path: `${iconDir}service-life.svg`
+    },
+    {
+        name: 'service_life_books',
+        path: `${iconDir}service-life-books.svg`
+    },
+    {
+        name: 'site',
+        path: `${iconDir}site.svg`
+    },
+    {
+        name: 'star',
+        path: `${iconDir}star.svg`
+    },
+    {
+        name: 'target',
+        path: `${iconDir}target.svg`
+    },
+    {
+        name: 'teams',
+        path: `${iconDir}teams.svg`
+    },
+    {
+        name: 'logo-2025',
+        path: `${iconDir}logo-2025.svg`
+    },
+    {
+        name: 'logo-title-2025',
+        path: `${iconDir}logo-title-2025.svg`
+    },
+    {
+        name: 'users',
+        path: `${iconDir}users.svg`
+    },
+    {
+        name: 'work_actions',
+        path: `${iconDir}work-actions.svg`
+    },
+    {
+        name: 'work_analysis',
+        path: `${iconDir}work-analysis.svg`
+    },
+    {
+        name: 'workflow',
+        path: `${iconDir}workflow.svg`
+    },
+    {
+        name: 'work_plan',
+        path: `${iconDir}work-plan.svg`
+    },
+    {
+        name: 'work_task',
+        path: `${iconDir}work-task.svg`
+    },
+    {
+        name: 'workspaces',
+        path: `${iconDir}workspaces.svg`
+    },
+    {
+        name: 'logo-add',
+        path: `${iconDir}logo-add.svg`
+    }
+];
